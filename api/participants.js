9b54bc9634d1b8ac95c5f4d4a3f7b147cf903bf9
Bruno Mokross <bruno@Brunos-MacBook-Pro.local>
2020-09-09 23:51:06 +0200

    install dotenv

--- api/participants.js
+++ api/participants.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const axios = require('axios')
 const app = express()
 const port = 3000
 
-const sheetID = "1tSMISVbPxIEHT2WVUWAj2A4_SFxTZMUmv_eE1YL1Ff0";
+const sheetID = process.env.GOOGLE_SHEET_ID;
 const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/1/public/values?alt=json`;
 const indexAPI = 'https://independentspaceindex.at/spaces.json';
 
