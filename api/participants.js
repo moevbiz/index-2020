8e91514755f5c5ffd311ec63662e3c9576cfcda2
Bruno Mokross <bruno@Brunos-MacBook-Pro.local>
2020-09-09 21:07:14 +0200

    try to work on vercel, add environment variables

--- api/participants.js
+++ api/participants.js
@@ -1,9 +1,72 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const sheetID = "1tSMISVbPxIEHT2WVUWAj2A4_SFxTZMUmv_eE1YL1Ff0";
+const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/1/public/values?alt=json`;
+const indexAPI = 'https://independentspaceindex.at/spaces.json';
+
+const checkResult = res => res.ok ? res.json() : Promise.resolve({});
+
+const join = (spaces, events) => {
+    let entries = [];
+    events.forEach(event =>  {
+        if (event.gsx$confirmed.$t !== "TRUE") return;
+        
+        let entry;
+        let space = spaces.find(space => space.name === event.gsx$space.$t) // check if space is in index API
+        if (space !== undefined) {
+            entry = {
+                space: space.name,
+                address: space.address,
+                district: space.district,
+                website: space.website,
+                events: [
+                    {
+                        title: event.gsx$title.$t,
+                        start: event.gsx$startdate.$t,
+                        end: event.gsx$enddate.$t,
+                        description: event.gsx$description.$t,
+                    }
+                ]
+            }
+        }
+        else {
+            entry = {
+                space: event.gsx$space.$t,
+                address: event.gsx$address.$t,
+                district: event.gsx$district.$t,
+                website: event.gsx$website.$t,
+            }
+        }
+
+        if (existingEntry = entries.find(prev => prev.space === entry.space)) {
+            existingEntry.events.push(entry.events[0])
+        }
+        else entries.push(entry)
+    })
+    return entries
+}
+
+function getSpaces() {
+    return axios.get(indexAPI);
+}
+  
+function getEvents() {
+    return axios.get(googleSheetUrl);
+}
+
+Promise.all([getSpaces(), getEvents()])
+.then(([spacesData, eventsData]) => {
+    console.log([spacesData, eventsData.feed.entry])
+    const result = join(spacesData, eventsData.feed.entry);
+    build(result);
+})
+.catch(err => console.error(err));
+
+app.get('/api/participants.js', (req, res) => {
+  res.send(result)
 })
 
 app.listen(port, () => {
